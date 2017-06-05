2ab159145421df950eeea6f8346cb558596d6ed1
Blasapo <blasapo@gmail.com>
2017-06-05 13:43:29 +0200

    basic structure

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function search () {
   var request = new XMLHttpRequest();
   var urlApi = "https://api.taiga.io/api/v1/issues";
 
-  request.open('GET', /*url*/, true);
+  request.open('GET', urlApi, true);
 
   request.onload = function() {
   };
