06e386320bf314520571fd0afc69e88b19dfa29d
Blasapo <blasapo@gmail.com>
2017-06-06 12:18:46 +0200

    Access issues API

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ function getIssuesData () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
             var projectId = data.id;
-
+            var urlApiIssues = "https://api.taiga.io/api/v1/issues?project=";
+            urlApiIssues = urlApiIssues + projectId;
         } else {
 
         }
