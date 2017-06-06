c55fee71d1245c4d465fc953f98e71a8cf693dc1
Blasapo <blasapo@gmail.com>
2017-06-06 12:56:54 +0200

    Print issues data

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 "use strict";
 var inputProject = document.querySelector(".js-input-slug");
 var searchBtn = document.querySelector(".js-btn-search");
+var ramon = document.querySelector(".js-results");
 
-function getIssuesData () {
-  var request = new XMLHttpRequest();
-  var slugName = inputProject.value;
-  var urlApiProject = "https://api.taiga.io/api/v1/projects/by_slug?slug=";
-  urlApiProject = urlApiProject + slugName;
+function getIssuesData() {
+    var requestProjectId = new XMLHttpRequest();
+    var slugName = inputProject.value;
+    var urlApiProject = "https://api.taiga.io/api/v1/projects/by_slug?slug=";
+    urlApiProject = urlApiProject + slugName;
 
-  request.open('GET', urlApiProject, true);
+    requestProjectId.open('GET', urlApiProject, true);
 
-  request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            var projectId = data.id;
+    requestProjectId.onload = function() {
+        if (requestProjectId.status >= 200 && requestProjectId.status < 400) {
+            var dataProject = JSON.parse(requestProjectId.responseText);
+            var projectId = dataProject.id;
             var urlApiIssues = "https://api.taiga.io/api/v1/issues?project=";
             urlApiIssues = urlApiIssues + projectId;
+
+            var requestProjectIssues = new XMLHttpRequest();
+            requestProjectIssues.open('GET', urlApiIssues, true);
+            requestProjectIssues.onload = function() {
+                if (requestProjectIssues.status >= 200 && requestProjectIssues.status < 400) {
+                    var dataIssues = JSON.parse(requestProjectIssues.responseText);
+                    ramon.innerHTML = dataIssues[0].subject;
+                } else {
+
+                }
+
+            };
+            requestProjectIssues.onerror = function() {
+
+            };
+            requestProjectIssues.send();
+
         } else {
 
         }
-  };
+    };
 
-  request.onerror = function() {
+    requestProjectId.onerror = function() {
 
-  };
-  request.send();
+    };
+    requestProjectId.send();
 }
 
 searchBtn.addEventListener('click', getIssuesData);
